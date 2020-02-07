bb7e1df7001f13ad013897d59e8133299bc363be
Edouard-Leroy <leroyedouard@protonmail.com>
2020-02-07 18:03:45 +0100

    Amelioration FINALE?? de 'resumePartie' dans Interface Jouable

--- src/main/java/Jeu/Jouable.java
+++ src/main/java/Jeu/Jouable.java
@@ -12,17 +12,17 @@ public interface Jouable {
     GameCard joue();
 
     default void resumePartie(GameCard gc) {
-        String resultat = "perdu";
+        //String resultat = "perdu";
         int tentative = gc.getScore()-1;
 
         if (gc.isGagne()) {
-            resultat = "gagne";
             System.out.println("Dommage !! Votre code a été trouvé en " +tentative+ " tentative(s)");
             System.out.println("L'ordinateur a été meilleur que vous !");
         }
         else if (!gc.isGagne())
-        System.out.println("L'ordinateur " + resultat + " en " + tentative + " tentative(s) \nFélicitation !! Votre code n'a pas été dechiffré !");
-        //System.out.println("Félicitation !! Votre code n'a pas été dechiffré !");
+            //resultat = "gagne";
+        System.out.println("L'ordinateur a perdu en " + tentative + " tentative(s) \nFélicitation !! Votre code n'a pas été dechiffré !");
+        //System.out.println("Félicitation !! Votre code n'a pas été dechiffré !"); dans ce cas cette phrase est affichee dans les 2 cas de figures Pourquoi ??
     }
 }
 
